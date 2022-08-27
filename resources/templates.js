3976ae221daf401be07de423004984e1c03c00ef
akless <andre.kless@web.de>
2022-08-27 16:06:58 +0200

    bugfix

--- resources/templates.js
+++ resources/templates.js
@@ -106,7 +106,7 @@ export function main( app, show_solution ) {
     <header class="bg-light border rounded-top d-flex flex-wrap justify-content-between align-items-center p-2">
 
       <!-- Heading -->
-      <div id="heading" class="p-2 pe-3" data-lang="main_${ heading }">${ app.text[ heading ] }</div>
+      <div id="heading" class="p-2 pe-3" data-lang="${ heading }">${ app.text[ heading ] }</div>
 
       <div class="d-flex align-items-center text-nowrap px-2" ?data-hidden=${ app.fixed_notation }>
 
